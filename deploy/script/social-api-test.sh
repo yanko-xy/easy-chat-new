3408a543c6a13abe550fc64968d9ef2bc9cc54bc
yanko <904852749@qq.com>
2024-09-28 16:26:48 +0800

    sync

--- deploy/script/social-api-test.sh
+++ deploy/script/social-api-test.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 
-reso_addr='registry.cn-hangzhou.aliyuncs.com/easy-chat-xy/social-api-dev'
+server_name="social"
+server_type="api"
+port=8889
+
+reso_addr="registry.cn-hangzhou.aliyuncs.com/easy-chat-xy/${server_name}-${server_type}-dev"
 tag='latest'
 
-container_name="easy-chat-social-api-test"
+
+container_name="easy-chat-${server_name}-${server_type}-test"
+
+pod_ip="120.26.209.19"
 
 docker stop ${container_name}
 
@@ -15,4 +23,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-chat -v /easy-chat/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 8889:8889 -v /root/easy-chat/social-api-logs:/social/logs/  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p ${port}:${port} -v /root/easy-chat/${server_name}-${server_type}-logs:/${server_name}/logs/ --name=${container_name} -d ${reso_addr}:${tag}
--- deploy/script/social-rpc-test.sh
+++ deploy/script/social-rpc-test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-reso_addr='registry.cn-hangzhou.aliyuncs.com/easy-chat-xy/social-rpc-dev'
+
+server_name="social"
+server_type="rpc"
+port=10001
+
+reso_addr="registry.cn-hangzhou.aliyuncs.com/easy-chat-xy/${server_name}-${server_type}-dev"
 tag='latest'
 
 
-
-container_name="easy-chat-social-rpc-test"
+container_name="easy-chat-${server_name}-${server_type}-test"
 
 pod_ip="120.26.209.19"
 
@@ -19,4 +23,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-chat -v /easy-chat/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10001:10001 -e POD_IP=${pod_ip} -v /root/easy-chat/social-rpc-logs:/social/logs/ --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p ${port}:${port} -e POD_IP=${pod_ip} -v /root/easy-chat/${server_name}-${server_type}-logs:/${server_name}/logs/ --name=${container_name} -d ${reso_addr}:${tag}
